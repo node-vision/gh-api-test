7f63dcf354af8304ca8b2f967309dcdc28241a04
romanmandryk <roman.mandryk@gmail.com>
2018-12-06 14:53:42 +1100

    removed .env

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const gh = new GitHub({ token: process.env.GH_TOKEN});
 
 async function getInfo(){
   try{
-    console.log('start', GitHub)
+    console.log('start')
     const ghUser =  gh.getUser();
     //const notifications = (await ghUser.listNotifications()).data;
     //const me = (await ghUser.getProfile()).data;
     //console.log('me', me, notifications.length);
     const ghRepo = gh.getRepo('node-vision', 'gh-api-test');
-    const pm = await ghRepo.writeFile('master', '/content/content1.json', `{"some": ""key}`, 'commit msg 1', {});
+    const pm = await ghRepo.writeFile('staging', 'content/subfolder/content3.json', `{"some": "content2"}`, 'commit msg 1', {});
   } catch (e){
     console.log(e)
   }
